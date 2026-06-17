1b134528d1b9b5718c7448ce0c4910fe0decd232
loupblanc548 <foulonleo8@gmail.com>
2026-06-17 23:48:07 +0200

    fix: add .js extensions to imports and remove allowImportingTsExtensions

--- src/services/psn.ts
+++ src/services/psn.ts
@@ -1,5 +1,5 @@
-import logger from "../utils/logger";
-import { config } from "../config";
+import logger from "../utils/logger.js";
+import { config } from "../config.js";
 
 import * as psnApi from "psn-api";
 
